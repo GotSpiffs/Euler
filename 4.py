9ea23ec5fb9ba473f6335f279f047dfa33874511
Luum Habtemariam <habtemlu@umich.edu>
2016-09-20 02:11:11 -0400

    k not as sucky again

--- 4.py
+++ 4.py
@@ -5,6 +5,5 @@ from itertools import combinations
 def ispalin(x):
 	return x == int(str(x)[::-1])
 
-l = [i*j for i,j in combinations(range(100,999), r=2) if ispalin(i*j)]
+max(i*j for i,j in combinations(range(100,999), r=2) if ispalin(i*j))
 
-print max(l)
--- 8.py
+++ 8.py
@@ -4,12 +4,6 @@ def multiply(x):
 	return reduce(lambda x, y: x*y, x)
 
 with open('8.txt','r') as file:
-    y = []
-    for line in file:
-        for i in list(line.strip()):
-            y.append(i)
+	y = [int(i) for line in file for i in list(line.strip()]
 
-y = [int(i) for i in y]
-l = [multiply(y[i:i+13]) for i in range(len(y) - 13)]
-
-print max(l)
+max(multiply(y[i:i+13]) for i in range(len(y) - 13))
