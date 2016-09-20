2b1fabccaec6fe08846c984bc59e668bea5fd09d
Luum Habtemariam <habtemlu@umich.edu>
2016-09-20 02:12:25 -0400

    k im going to sleep now

--- 8.py
+++ 8.py
@@ -4,6 +4,6 @@ def multiply(x):
 	return reduce(lambda x, y: x*y, x)
 
 with open('8.txt','r') as file:
-	y = [int(i) for line in file for i in list(line.strip()]
+	y = [int(i) for line in file for i in list(line.strip())]
 
 max(multiply(y[i:i+13]) for i in range(len(y) - 13))
